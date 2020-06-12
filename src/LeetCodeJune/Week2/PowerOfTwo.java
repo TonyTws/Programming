f7c02bc4cc2a0138299e168c4bed6f07bcfd4e09
Shankar Chakraborty <40225075+TonyTws@users.noreply.github.com>
2020-06-13 01:30:54 +0530

    Added Leetcode:june - week 2

---
+++ src/LeetCodeJune/Week2/PowerOfTwo.java
@@ -0,0 +1,13 @@
+package LeetCodeJune.Week2;
+
+public class PowerOfTwo {
+    public static void main(String[] args) {
+        System.out.println(isPowerOfTwo(3));
+    }
+    public static boolean isPowerOfTwo(int n) {
+        System.out.println(n);
+        if(n == 0) return true;
+        return isPowerOfTwo(n/2);
+
+    }
+}
