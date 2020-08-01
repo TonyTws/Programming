45c9a92c37490a4aa7e42a5349fea15a47672efc
Shankar Chakraborty <40225075+TonyTws@users.noreply.github.com>
2020-08-01 23:31:32 +0530

    added hashing day 31 questions

--- src/Extra/LeetCodeAugust/Week1/DetectCapital.java
+++ src/Extra/LeetCodeAugust/Week1/DetectCapital.java
@@ -2,7 +2,7 @@ package Extra.LeetCodeAugust.Week1;
 
 public class DetectCapital {
     public static void main(String[] args) {
-        String[] str = new String[]{"USA","leetcode","Google"};
+        String[] str = new String[]{"USA", "leetcode", "Google"};
         for (String s : str) {
             System.out.println(detectCapitalUse(s));
         }
@@ -12,23 +12,22 @@ public class DetectCapital {
      * if caps is equal to length of string
      * if caps=1 and the 1st letter in the string is capital
      * if there are no capital letters in the string
-     *
      */
 
     public static boolean detectCapitalUse(String word) {
-        int caps  =0;
-        for (int i = 0; i < word.length() ; i++) {
-            if(word.charAt(i) - 'A' < 26)
+        int caps = 0;
+        for (int i = 0; i < word.length(); i++) {
+            if (word.charAt(i) - 'A' < 26)
                 caps++;
         }
 
-        if(caps == word.length()){
+        if (caps == word.length()) {
             return true;
-        }else if(caps ==1 && word.charAt(0) - 'A' < 26){
+        } else if (caps == 1 && word.charAt(0) - 'A' < 26) {
             return true;
-        }else if(caps == 0){
+        } else if (caps == 0) {
             return true;
-        }else{
+        } else {
             return false;
         }
     }
